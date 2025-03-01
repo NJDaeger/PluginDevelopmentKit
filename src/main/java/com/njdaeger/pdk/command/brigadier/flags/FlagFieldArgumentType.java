6c50e6884db5838936c43997bdd1fe7e0cd6d8bf
Noah Daeger <noahdaeger@hotmail.com>
2025-03-01 09:35:29 -0500

    More flag stuff.. still needs work

--- src/main/java/com/njdaeger/pdk/command/brigadier/flags/FlagFieldArgumentType.java
+++ src/main/java/com/njdaeger/pdk/command/brigadier/flags/FlagFieldArgumentType.java
@@ -9,11 +9,9 @@ import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import com.njdaeger.pdk.command.brigadier.arguments.BasePdkArgumentType;
-import com.njdaeger.pdk.command.flag.Flag;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -30,7 +28,8 @@ public class FlagFieldArgumentType extends BasePdkArgumentType<FlagMap, String> 
 
     @Override
     public @NotNull <S> CompletableFuture<Suggestions> listSuggestions(@NotNull CommandContext<S> context, @NotNull SuggestionsBuilder builder) {
-        var splitInput = builder.getRemaining().trim().split(" ");
+        var input = builder.getRemaining().trim();
+        var splitInput = input.split(" ");
         var currentFlagIndex = -1;
         IPdkCommandFlag<?> currentFlag = null;
         for (var i = splitInput.length - 1; i >= 0; i--) {
@@ -42,118 +41,22 @@ public class FlagFieldArgumentType extends BasePdkArgumentType<FlagMap, String> 
             }
         }
 
-        System.out.println("CurrentFlagIndex: " + currentFlagIndex);
-        System.out.println("CurrentFlag: " + currentFlag);
-        System.out.println("CurrentInput: '" + builder.getRemaining() + "'");
-        System.out.println("SplitInput: " + Arrays.toString(splitInput));
-        System.out.println("RemainingInput: '" + builder.getInput() + "'");
-
-        var inputAfterFlag = String.join(" ", Arrays.asList(splitInput).subList(currentFlagIndex + 1, splitInput.length));
-        var currentWord = splitInput[splitInput.length - 1];
-        System.out.println("CurrentWord: " + currentWord);
-        System.out.println("RemainingInput: " + inputAfterFlag);
-
-        if (currentWord.startsWith("-") && !builder.getRemaining().endsWith(" ")) {
-            getUnusedFlags(context).forEach(flag -> builder.suggest("-" + flag.getName(), flag.getTooltipAsMessage()));
-            return builder.buildFuture();
-        }
-
-//        var currentWord = splitInput[splitInput.length - 1];
-//        var previousWord = splitInput.length > 1 ? splitInput[splitInput.length - 2] : null;
-
-        //push the completion cursor to the start of the current word
-        var completingAt = builder.getStart() + inputAfterFlag.length() +
-                (currentFlag == null
-                    ? 0
-                    : builder.getRemaining().lastIndexOf("-" + currentFlag.getName()) + currentFlag.getName().length() + 1);
-        var newBuilder = builder.createOffset(completingAt);
-
-        if (currentFlag == null || currentFlag.isBooleanFlag() || (!inputAfterFlag.isBlank() && builder.getRemaining().endsWith(" "))) {
-            getUnusedFlags(context).forEach(flag -> newBuilder.suggest("-" + flag.getName(), flag.getTooltipAsMessage()));
+        if (currentFlag == null || currentFlag.isBooleanFlag() || (currentFlagIndex < splitInput.length - 1 && builder.getRemaining().endsWith(" "))) {
+            var newBuilder = builder.createOffset(builder.getStart() + builder.getRemaining().length());
+            var currentWord = splitInput[splitInput.length - 1];
+            getUnusedFlags(context).forEach(flag -> newBuilder.suggest((currentWord.startsWith("-") ? "" : "-") + flag.getName(), flag.getTooltipAsMessage()));
             return newBuilder.buildFuture();
         }
 
-        System.out.println("CompletingAt: " + (completingAt + (builder.getRemaining().endsWith(" ") ? 0 : 1)));
+        var valueStart = input.indexOf('-' + currentFlag.getName());
+        while (valueStart < input.length() && Character.isWhitespace(input.charAt(valueStart))) valueStart++;
 
-        return currentFlag.getType().listSuggestions(context, newBuilder.createOffset(completingAt + (builder.getRemaining().endsWith(" ") ? 0 : 1)));
-//
-//        //attempt to get the current flag
-////        var currentFlag2 = getFlag(currentWord.trim());
-//
-//        //if the current flag wasnt found from the current word, attempt to get the flag from the previous word if possible
-//        //if the current input ends with a space, we assume they are not trying to complete the flag data anymore, and we should suggest all currently possible flags
-//        if (currentFlag == null && previousWord != null && !builder.getRemaining().endsWith(" ")) {
-//            currentFlag2 = getFlag(previousWord.trim());
-//        }
-//
-//        if (currentFlag2 == null || currentFlag2.isBooleanFlag()) {
-//            getUnusedFlags(context).forEach(flag -> newBuilder.suggest("-" + flag.getName(), flag.getTooltipAsMessage()));
-//            return newBuilder.buildFuture();
-//        }
-//
-//        //return currentFlag.getType().listSuggestions(context, newBuilder);
-//
-//        //find the current flag by looking backwards until we find a flag
-//        var currentFlagIndex = -1;
-//        IPdkCommandFlag<?> currentFlag;
-//        for (var i = splitInput.length - 1; i >= 0; i--) {
-//            var foundFlag = getFlag(splitInput[i]);
-//            if (foundFlag != null) {
-//                currentFlagIndex = i;
-//                currentFlag = foundFlag;
-//                break;
-//            }
-//        }
-//
-//        //merge the remaining input after the flag into a single string
-//        var remainingInput = String.join(" ", Arrays.asList(splitInput).subList(currentFlagIndex + 1, splitInput.length));
-//
-//        var
-
+        var newBuilder = builder.createOffset(builder.getStart() + valueStart + currentFlag.getName().length() + 2);
+        return currentFlag.getType().listSuggestions(context, newBuilder);
     }
 
     @Override
     public FlagMap convertToCustom(String nativeType, StringReader reader) throws CommandSyntaxException {
-//        var map = new FlagMap();
-////        var splitArgs = nativeType.split(" ");
-////        for (var flag : flags) {
-////            for (var i = 0; i < splitArgs.length; i++) {
-////                var currentArg = splitArgs[i];
-////                if (!currentArg.equalsIgnoreCase("-" + flag.getName())) continue;
-////
-////                if (!flag.isBooleanFlag()) {
-////                    if (splitArgs.length <= i + 1) {
-////                        throw FLAG_NOT_BOOLEAN.createWithContext(reader, flag.getName(), flag.getType().);
-////                        throw new CommandSyntaxException(null, () -> "Flag -" + flag.getName() + " requires a value.");
-////                    }
-////                    var flagValue = splitArgs[i + 1];
-////                    map.setFlag(flag.getName(), flag.getType().parse(new StringReader(flagValue)));
-////                }
-////                else map.setFlag(flag.getName(), true);
-////            }
-////        }
-//
-//        System.out.println(reader.getString());
-//        System.out.println(reader.getRemainingLength());
-//        System.out.println(reader.canRead());
-//        System.out.println(reader.getCursor());
-//
-//        while (reader.getRemainingLength() > 0) {
-//            var currentArg = reader.readString();
-//            System.out.println("CurrentArg: " + currentArg);
-//            if (currentArg.isBlank()) continue;
-//            var flag = getFlag(currentArg);
-//            if (flag == null) throw UNKNOWN_FLAG.createWithContext(reader, currentArg);
-//            if (!flag.isBooleanFlag()) {
-//                if (!reader.canRead()) throw MISSING_ARGUMENT.createWithContext(reader, flag.getName());
-//                var flagValue = flag.getType().parse(reader);
-//                System.out.println("FlagValue: " + flagValue);
-//                map.setFlag(flag.getName(), flagValue);
-//            }
-//            else map.setFlag(flag.getName(), true);
-//        }
-//
-//        return map;
         return parse(reader);
     }
 
@@ -161,25 +64,26 @@ public class FlagFieldArgumentType extends BasePdkArgumentType<FlagMap, String> 
     public @NotNull FlagMap parse(@NotNull StringReader reader) throws CommandSyntaxException {
         var map = new FlagMap();
         while (reader.canRead()) {
-            var currentArg = reader.readString();
-            if (currentArg.isBlank()) {
+            reader.skipWhitespace();
+            if (reader.peek() != '-') {
+                break;
+            } else {
                 reader.skip();
-                continue;
+                var flagName = readFlagName(reader);
+                var flag = getFlag(flagName);
+
+                if (flag == null) throw UNKNOWN_FLAG.createWithContext(reader, flagName);
+
+                reader.skipWhitespace();
+
+                if (!flag.isBooleanFlag()) {
+                    if (!reader.canRead()) throw MISSING_ARGUMENT.createWithContext(reader, flag.getName());
+                    var flagValue = flag.getType().parse(reader);
+                    map.setFlag(flag.getName(), flagValue);
+                }
+                else map.setFlag(flag.getName(), true);
+                reader.skipWhitespace();
             }
-            System.out.println("CurrentArg: " + currentArg);
-            var flag = getFlag(currentArg);
-            if (flag == null) throw UNKNOWN_FLAG.createWithContext(reader, currentArg);
-            if (!flag.isBooleanFlag()) {
-                if (!reader.canRead()) throw MISSING_ARGUMENT.createWithContext(reader, flag.getName());
-                var nextArg = reader.readString();
-                System.out.println("PreParse: " + reader.getCursor());
-                var flagValue = flag.getType().parse(reader);
-                System.out.println("PostParse: " + reader.getCursor());
-                System.out.println("NextArg: " + nextArg);
-                System.out.println("FlagValue: " + flagValue);
-                map.setFlag(flag.getName(), flagValue);
-            }
-            else map.setFlag(flag.getName(), true);
         }
         return map;
     }
@@ -202,6 +106,14 @@ public class FlagFieldArgumentType extends BasePdkArgumentType<FlagMap, String> 
             }
         }
         return null;
+    }
+
+    private String readFlagName(StringReader reader) throws CommandSyntaxException {
+        int start = reader.getCursor();
+        while (reader.canRead() && !Character.isWhitespace(reader.peek())) {
+            reader.skip();
+        }
+        return reader.getString().substring(start, reader.getCursor());
     }
 
     private <S> List<IPdkCommandFlag<?>> getUnusedFlags(CommandContext<S> context) {
